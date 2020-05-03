2d67f5b7b34f102815941db143e6d8ef8dc0dadc
juancarbajaltxt <juan@urbantxt.com>
2020-05-02 17:09:00 -0700

    added CSS challenge. Js challenge in the works

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -8,5 +8,27 @@ import { Component } from '@angular/core';
 export class Tab3Page {
 
   constructor() {}
+  // public x = 0;
+
+  // click(){
+  //   document.getElementById('bulb-button').addEventListener('click', function(){
+  //   console.log('x');
+  //   this.x += 1;
+  //   if (x==5){
+  //       document.getElementById("picture").style.backgroundImage = 'none';
+  //   }
+  //   else {
+  //       if (x <5){
+  //           if (x%2==1){
+  //               document.getElementById("picture").style.backgroundImage = 'url("/../assets/bulboff.png")';
+  //           }
+  //           if (x%2==0){
+  //               document.getElementById("picture").style.backgroundImage = 'url("/../assets/bulbon.png")';
+  //           }
+  //       }
+  //       else {}
+  //   }
+  //  });
+  // }
 
 }
